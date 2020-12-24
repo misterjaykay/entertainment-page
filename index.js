9a508cbee1103a0856e383871c52dc5c98557948
Jonghyun Kang <misterjaykay@gmail.com>
2020-12-24 09:15:52 -0500

    initial commit & basic carousel

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.your-class').slick({
+        arrows: false,
+        infinite: true,
+        initialSlide: 0,
+        autoplay: true,
+        autoplaySpeed: 2000
+  });
+});
