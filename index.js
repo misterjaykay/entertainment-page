246d25dc6c01280e42e777842bf4d18b5758ef98
Jonghyun Kang <misterjaykay@gmail.com>
2020-12-24 10:48:15 -0500

    scss and slider commit

--- index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.your-class').slick({
-        arrows: false,
-        infinite: true,
-        initialSlide: 0,
-        autoplay: true,
-        autoplaySpeed: 2000
-  });
-});
---
+++ public/assets/js/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".my-slider").slick({
+    arrows: false,
+    infinite: true,
+    // initialSlide: 0,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true
+  });
+});
