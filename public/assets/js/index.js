9176c8af7849054e3372962802b011b365f389b6
Jonghyun Kang <misterjaykay@gmail.com>
2020-12-24 16:41:14 -0500

    artist profile and company text

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,10 +1,13 @@
 $(document).ready(function () {
   $(".my-slider").slick({
-    arrows: false,
+    // arrows: false,
     infinite: true,
     // initialSlide: 0,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    fade: true
+    // autoplay: true,
+    // autoplaySpeed: 3000,
+    fade: true,
+    draggable: false,
+    prevArrow: `<button type="button" class="slick-prev"> <<< </button>`,
+    nextArrow: `<button type="button" class="slick-next"> >>> </button>`
   });
 });
